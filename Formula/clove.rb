343cdb6389e986d283b2141ed8ff9ecc12272108
Emanuele Libralato <e.libralato@gmail.com>
2026-03-16 23:53:17 +0100

    fix: missing bun deps

--- Formula/clove.rb
+++ Formula/clove.rb
@@ -8,7 +8,7 @@ class Clove < Formula
   url "https://github.com/10ko/clove/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
-  depends_on "bun"
+  depends_on "oven-sh/bun/bun"
 
   def install
     libexec.install Dir["*"]
--- README.md
+++ README.md
@@ -5,4 +5,11 @@ brew tap 10ko/clove
 brew install clove
 ```
 
+Via SSH:
+
+```bash
+brew tap 10ko/clove git@github.com:10ko/homebrew-clove.git
+brew install clove
+```
+
 See [github.com/10ko/clove](https://github.com/10ko/clove) for the project.
